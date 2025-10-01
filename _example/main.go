4d8030f96660b4277f9fec990f1e3dcb71960a3e
Angelo Fallaria <ba.fallaria@gmail.com>
2025-10-01 10:34:18 +0800

    docs: add example file

---
+++ _example/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/angelofallars/payrex-go"
+)
+
+func main() {
+	payrexClient := payrex.NewClient("sk_...")
+
+	paymentIntent, err := payrexClient.PaymentIntents.Retrieve("pi_...")
+	if err != nil {
+		printPayrexError(err)
+		return
+	}
+
+	fmt.Println(paymentIntent)
+
+	paymentIntent, err = payrexClient.PaymentIntents.Create(&payrex.CreatePaymentIntentOptions{
+		Amount:         10000,
+		Description:    payrex.Optional("Dino Treat"),
+		PaymentMethods: []payrex.PaymentMethod{payrex.PaymentMethodGCash},
+	})
+	if err != nil {
+		printPayrexError(err)
+		return
+	}
+
+	fmt.Println(paymentIntent)
+}
+
+// Handle errors
+func printPayrexError(err error) {
+	var payrexError payrex.Error
+	if !errors.As(err, &payrexError) {
+		return
+	}
+
+	for _, errMsg := range payrexError.Errors {
+		fmt.Printf("code: %v\n", errMsg.Code)
+		fmt.Printf("detail: %v\n", errMsg.Detail)
+		fmt.Printf("parameters: %v\n", errMsg.Parameter)
+	}
+}
