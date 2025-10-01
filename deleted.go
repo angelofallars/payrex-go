6943fcd4854bd197b9f99f0ba719c9bf8110a3d6
Angelo Fallaria <ba.fallaria@gmail.com>
2025-10-01 11:32:14 +0800

    docs(resource): add doc comment for `DeletedResource.ID` field

--- deleted.go
+++ deleted.go
@@ -2,6 +2,7 @@ package payrex
 
 // DeletedResource represents a resource of any type that has been deleted.
 type DeletedResource struct {
+	// Unique identifier for the resource.
 	ID      string `json:"id"`
 	Deleted bool   `json:"deleted"`
 }
