dfc141972e02f23ff84ad5300ff7a194b45810f3
Angelo Fallaria <ba.fallaria@gmail.com>
2025-10-02 15:25:45 +0800

    refactor(query): inline recursive parsing all in one function

--- internal/query/encode.go
+++ internal/query/encode.go
@@ -10,67 +10,59 @@ import (
 // Encode returns a URL encoded version of a struct value
 // using `query:"<value>"` tags.
 func Encode(params any) string {
-	return buildQueryRecursive(params, "").Encode()
+	return parseQueryParams(url.Values{}, "", reflect.ValueOf(params)).Encode()
 }
 
-func buildQueryRecursive(params any, parentKey string) url.Values {
-	queryParams := url.Values{}
-
-	paramsValue := reflect.ValueOf(params)
-	if paramsValue.Kind() == reflect.Pointer {
-		paramsValue = paramsValue.Elem()
-	}
-
-	paramsType := paramsValue.Type()
-
-	for i := range paramsValue.NumField() {
-		field := paramsType.Field(i)
-		key := field.Tag.Get("query")
-		if key == "" {
-			panic(fmt.Sprintf("'query' tag on struct field '%s.%s' not set",
-				paramsType.Name(), field.Name))
-		}
-
-		newKey := key
-		if parentKey != "" {
-			newKey = fmt.Sprintf("%s[%s]", parentKey, key)
-		}
-
-		value := paramsValue.Field(i)
-		addQueryValue(queryParams, newKey, value)
-	}
-
-	return queryParams
-}
-
-func addQueryValue(queryParams url.Values, key string, value reflect.Value) {
+func parseQueryParams(queryParams url.Values, key string, value reflect.Value) url.Values {
 	switch value.Kind() {
 	case reflect.Struct:
-		for k, vs := range buildQueryRecursive(value.Interface(), key) {
-			for _, v := range vs {
-				queryParams.Add(k, v)
+		valueType := value.Type()
+		for i := range value.NumField() {
+			field := valueType.Field(i)
+
+			tagKey := field.Tag.Get("query")
+			if tagKey == "" {
+				panic(fmt.Sprintf("'query' tag on struct field '%s.%s' not set",
+					valueType.Name(), field.Name))
 			}
+
+			fieldKey := tagKey
+			if key != "" {
+				fieldKey = fmt.Sprintf("%s[%s]", key, tagKey)
+			}
+			fieldValue := value.Field(i)
+
+			parseQueryParams(queryParams, fieldKey, fieldValue)
 		}
 	case reflect.Slice:
 		for i := range value.Len() {
 			elem := value.Index(i)
 			elemKey := fmt.Sprintf("%s[]", key)
-			addQueryValue(queryParams, elemKey, elem)
+
+			parseQueryParams(queryParams, elemKey, elem)
 		}
 	case reflect.Map:
-		for _, k := range value.MapKeys() {
-			v := value.MapIndex(k)
-			newKey := fmt.Sprintf("%s[%s]", key, k.String())
-			addQueryValue(queryParams, newKey, v)
+		iter := reflect.ValueOf(value).MapRange()
+		for iter.Next() {
+			k, v := iter.Key(), iter.Value()
+
+			mapKey := k.String()
+			if key != "" {
+				mapKey = fmt.Sprintf("%s[%s]", key, k.String())
+			}
+
+			parseQueryParams(queryParams, mapKey, v)
 		}
 	case reflect.Pointer:
 		// Null values will not be added to the query
 		if value.IsZero() {
-			return
+			return queryParams
 		}
 
-		addQueryValue(queryParams, key, value.Elem())
+		parseQueryParams(queryParams, key, value.Elem())
 	default:
 		queryParams.Add(key, fmt.Sprintf("%v", value))
 	}
+
+	return queryParams
 }
